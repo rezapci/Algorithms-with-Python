d9107bebf8384d37b5ac0a44405aec56cce17b58
Reza Hashemi <rezapci@msn.com>
2019-09-03 15:56:27 -0400

    Update less1_task3.py

--- less1_task3.py
+++ less1_task3.py
@@ -1,18 +1,18 @@
-# Ссылка на блоксхемы:
+# Reference to Flowcharts:
 # https://drive.google.com/file/d/1MxReXi2rVMhkFsbYynmA2gXycnVpdrwP/view?usp=sharing
 
-# По введенным пользователем координатам двух точек вывести уравнение прямой, проходящей через эти точки.
+# Using the coordinates of two points entered by the user, derive the equation of a line passing through these points.
 
-print("Выведем уравнение прямой, проходящей через заданные точки")
-print("Введите координаты первой точки: ")
+print ( "We derive the equation of a line passing through given points " )
+print ( " Enter the coordinates of the first point: " )
 x1 = float(input("x1 = "))
 y1 = float(input("y1 = "))
 
-print("Введите координаты второй точки: ")
+print ( " Enter the coordinates of the second point: " )
 x2 = float(input("x2 = "))
 y2 = float(input("y2 = "))
 
 k = (y1 - y2) / (x1 - x2)
 b = y2 - k*x2
 
-print("Уравнение прямой, проходящей через эти точки: y = {} * x + {}".format(k, b))
+print ( " The equation of a line passing through these points: y = {} * x + {} " .format (k, b))
