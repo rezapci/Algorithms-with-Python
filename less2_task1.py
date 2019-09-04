e047d82b0b992722be3c67f4f74fa46e20761296
Reza Hashemi <rezapci@msn.com>
2019-09-04 13:04:13 -0400

    Update less2_task1.py

--- less2_task1.py
+++ less2_task1.py
@@ -1,22 +1,22 @@
-# Ссылка на блок-схемы:
+# Link to flowcharts:
 # https://drive.google.com/file/d/12xTQSyUeeSIWUDkwn3nWW-KHMmj31Rxy/view?usp=sharing
 
-# Написать программу, которая будет складывать, вычитать, умножать или делить два числа.
-# Числа и знак операции вводятся пользователем. После выполнения вычисления программа не должна завершаться,
-# а должна запрашивать новые данные для вычислений.
-# Завершение программы должно выполняться при вводе символа '0' в качестве знака операции.
-# Если пользователь вводит неверный знак (не '0', '+', '-', '*', '/'), то программа должна сообщать ему об ошибке и
-# снова запрашивать знак операции.
-# Также сообщать пользователю о невозможности деления на ноль, если он ввел 0 в качестве делителя.
+# Write a program that will add, subtract, multiply or divide two numbers.
+# Numbers and the operation sign are entered by the user. After the calculation is complete, the program should not end,
+# and should request new data for calculations.
+# Program termination should be performed when the character '0' is entered as an operation sign.
+# If the user enters an incorrect character (not '0', '+', '-', '*', '/'), then the program should inform him of the error and
+# request the operation sign again.
+# Also inform the user about the impossibility of dividing by zero if he entered 0 as a divisor.
 
 
-print("Введите два числа и операцию над ними:")
+print ( " Enter two numbers and the operation on them: " )
 operation = None
 
 while operation != "0":
-    number1 = float(input("Первое число: "))
-    number2 = float(input("Второе число: "))
-    operation = input("Что будем с ними делать (+, -, *, /): ")
+    number1 = float(input("first number: "))
+    number2 = float(input("second number: "))
+    operation =  input ( " What will we do with them (+, -, *, /): " )
 
     if operation == "+":
         print(number1 + number2)
@@ -26,10 +26,10 @@ while operation != "0":
         print(number1 * number2)
     elif operation == "/":
         if number2 == 0:
-            print("Деление на ноль невозможно!")
+            print ( " Division by zero is not possible! " )
         else:
             print(number1 / number2)
     else:
-        print("Неизвестная операция")
+        print ( " Unknown operation " )
 else:
-    print("До свидания")
+    print ( " Goodbye " )
