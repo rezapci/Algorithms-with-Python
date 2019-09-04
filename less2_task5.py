ad8a710baf282758d09b69883f3c9a2b7f37ed25
Reza Hashemi <rezapci@msn.com>
2019-09-04 13:16:53 -0400

    Update less2_task5.py

--- less2_task5.py
+++ less2_task5.py
@@ -1,10 +1,10 @@
-# Ссылка на блок-схемы:
+# Link to flowcharts:
 # https://drive.google.com/file/d/12xTQSyUeeSIWUDkwn3nWW-KHMmj31Rxy/view?usp=sharing
 
-# Вывести на экран коды и символы таблицы ASCII, начиная с символа под номером 32 и заканчивая 127-м включительно.
-# Вывод выполнить в табличной форме: по десять пар «код-символ» в каждой строке.
+# Display the codes and characters of the ASCII table, starting with the character at number 32 and ending with the 127th inclusive.
+# Execute the output in tabular form: ten pairs of "code-symbol" in each line.
 
-for i in range(32, 128):
+for i in  range ( 32 , 128 ):
     print("{} - {}".format(i, chr(i)), end=' ')
     if i % 10 == 0:
         print()
