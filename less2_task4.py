931627cbb5902bfcb41a46bd4818cadb87dd84a6
Reza Hashemi <rezapci@msn.com>
2019-09-04 13:15:04 -0400

    Update less2_task4.py

--- less2_task4.py
+++ less2_task4.py
@@ -1,16 +1,16 @@
-# Ссылка на блок-схемы:
+# Link to flowcharts:
 # https://drive.google.com/file/d/12xTQSyUeeSIWUDkwn3nWW-KHMmj31Rxy/view?usp=sharing
 
-# Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...
-# Количество элементов (n) вводится с клавиатуры.
+# Find the sum of n elements of the following series of numbers: 1 -0.5 0.25 -0.125 ...
+# The number of items (n) is entered from the keyboard.
 
-print("Найдем сумму некоторого количества элементов ряда чисел")
-n = int(input("Введите количество элементов: "))
+print( "Find the sum of a certain number of elements in a series of numbers")
+n =  int (input("Enter the number of elements:"))
 
 x = 1
-y = 0
+y =  0
 
 for i in range(n):
     y = x + y
     x = x / -2
-print("Сумма {}".format(y))
+print("amount {}".format(y))
