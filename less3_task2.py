a46af36b255243a290a0b822659d74d0c62a4f02
Reza Hashemi <rezapci@msn.com>
2019-09-04 14:51:14 -0400

    Update less3_task2.py

--- less3_task2.py
+++ less3_task2.py
@@ -1,7 +1,7 @@
-# Во втором массиве сохранить индексы четных элементов первого массива.
-# Например, если дан массив со значениями 8, 3, 15, 6, 4, 2,
-# то во второй массив надо заполнить значениями 0, 3, 4, 5 (индексация начинается с нуля),
-# т.к. именно в этих позициях первого массива стоят четные числа.
+# In the second array, keep the indices of even elements of the first array.
+# For example, if an array is given with the values ​​8, 3, 15, 6, 4, 2,
+# then in the second array you need to fill in the values ​​0, 3, 4, 5 (indexing starts from zero),
+# tk exactly in these positions of the first array are even numbers.
 
 arr_1 = [8, 3, 15, 6, 4, 2]
 arr_2 = []
